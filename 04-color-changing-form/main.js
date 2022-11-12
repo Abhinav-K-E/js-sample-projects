a5cb58d5325a3958e649dc9014d3f3aefa0cf9eb
Abhinav <abhinavke091@gmail.com>
2022-11-12 21:00:19 +0700

    04-color-changing 1st commit

---
+++ 04-color-changing-form/main.js
@@ -0,0 +1,27 @@
+function myColor01 (){
+    document.getElementById("btn").style.background="#00539CFF";
+    document.querySelector("#wrapp").style.background="#00539CFF";
+    document.getElementsByClassName("inp01")[0].style.borderBottomColor="#00539CFF";
+    document.getElementsByClassName("inp01")[1].style.borderBottomColor="#00539CFF";
+}
+
+myColor02 = () =>{
+    document.getElementById("btn").style.background="#EA738DFF";
+    document.querySelector("#wrapp").style.background="#EA738DFF";
+    document.getElementsByClassName("inp01")[0].style.borderBottomColor="#EA738DFF";
+    document.getElementsByClassName("inp01")[1].style.borderBottomColor="#EA738DFF";
+}
+
+myColor03 = () =>{
+    document.getElementById("btn").style.background="#2BAE66FF";
+    document.querySelector("#wrapp").style.background="#2BAE66FF";
+    document.getElementsByClassName("inp01")[0].style.borderBottomColor="#2BAE66FF";
+    document.getElementsByClassName("inp01")[1].style.borderBottomColor="#2BAE66FF";
+}
+
+myColor04 = () =>{
+    document.getElementById("btn").style.background="#FFD662FF";
+    document.querySelector("#wrapp").style.background="#FFD662FF";
+    document.getElementsByClassName("inp01")[0].style.borderBottomColor="#FFD662FF";
+    document.getElementsByClassName("inp01")[1].style.borderBottomColor="#FFD662FF";
+}
